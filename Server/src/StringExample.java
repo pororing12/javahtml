19308c0958035110264e41a7e0c73b282f063745
pororing12 <42339050+pororing12@users.noreply.github.com>
2019-06-17 17:48:47 +0900

    06-17

---
+++ Server/src/StringExample.java
@@ -0,0 +1,11 @@
+
+public class StringExample {
+	public static void main(String[] args) {
+		
+		byte v1 = 0b0000011;
+		float f= 0.1234f;
+		
+		System.out.println(f);
+		System.out.println(v1);
+	}
+}
